a37da7644a59cda61d8612dcc55937f79247e0e7
devstar1992 <75045062+devstar1992@users.noreply.github.com>
2022-08-13 10:29:30 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+//endpoint
+const endpoint = {
+  http:"https://rinkeby.infura.io/v3/"
+};
+//web3
+const Web3 = require("web3");
+const { pretty } = require("pretty-bitte");
+const ethers = require('ethers');
+const web3 = new Web3(new Web3.providers.HttpProvider(endpoint.http));
+const { JsonRpcProvider } = require("@ethersproject/providers");
+const provider = new JsonRpcProvider(endpoint.http);
+//ABI
+const nftABI = require("./abi/nft.json");
+//wallets
+const {wallets, nftAddress} = require("./config.json");
+
+const run = async () => {
+  console.log('wallets',wallets);
+  for(let i = 0; i < wallets.length; i++){
+    _mint(wallets[i]);
+  }
+};
+const _mint = async (wallet) => {
+    try{
+      const signer = new ethers.Wallet(wallet, provider);
+      const contractInstance = new ethers.Contract(nftAddress, nftABI, signer);
+      const gasTx ={ gasLimit: 300000, gasPrice: convertToHex(10)};
+      const tx = await contractInstance.mint( convertToHex(1),gasTx);
+      const txHash = tx.hash;
+      console.log(`--Tx: ${txHash}`);
+      const receipt = await tx.wait();
+      console.log(`--Tx was mined in block: ${receipt.blockNumber}`);
+    }catch(e){
+      console.log('Error in _mint',e);
+    }
+};
+function convertToHex( value ){
+  if(value===NaN || value===false) return false;
+  let maxDep = 70;
+  let number = Number(value);
+  let decimal = 0;
+  while(maxDep>0){
+      maxDep --;
+      if(number < 10) {
+          return ethers.utils.parseUnits(String(Number(number).toFixed(decimal)), decimal).toHexString()
+      }
+      else{
+          number = number/10;
+          decimal++;
+      }
+  }
+  return false;
+}
+run();
